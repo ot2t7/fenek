f0f4266c4869f8e3d5b2a10a806996c01e3d460c
ot2t7 <ot2t7@riseup.net>
2022-05-28 20:00:29 -0500

    worked on finding vesica piscis height

--- src/cruise.rs
+++ src/cruise.rs
@@ -6,8 +6,15 @@ use geo_types::coord;
 use geo::{Line, Coordinate};
 use geo::algorithm::line_intersection::{line_intersection, LineIntersection};
 
-fn find_vesica_line(circle1: &Circle, circle2: &Circle) {
+fn find_vesica_line(circle1: &Circle, circle2: &Circle, d_craft: f32) {
+    // Following variable names may be taken from this graphic:
+    // https://diego.assencio.com/images/mathematics/circle-circle-intersection.svg
 
+    // Find d1 and d2 using a formula provided.
+    let d1 = (circle1.radius.powf(2.0) - circle2.radius.powf(2.0) + d_craft.powf(2.0)) / (2.0 * d_craft);
+    let d2 = d_craft - d1;
+
+    let vesica_height_top = (circle1.radius.powf(2.0) - d1.powf(2.0)).sqrt();
 }
 
 pub fn intercept(intercepted_craft: &Craft, controlled_craft: &mut Craft) {
@@ -23,5 +30,5 @@ pub fn intercept(intercepted_craft: &Craft, controlled_craft: &mut Craft) {
     // Both circles will overlap with eachother, which forms a Vesica
     // piscis (https://en.wikipedia.org/wiki/Vesica_piscis). We need
     // to find the line which represents the height of this Vesica piscis.
-    let vesica_line = find_vesica_line(&ic_circle, &cc_circle);
+    let vesica_line = find_vesica_line(&ic_circle, &cc_circle, d_craft);
 }
